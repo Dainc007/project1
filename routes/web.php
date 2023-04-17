e679726701b5d155055e9cc6f9a7d364c9304103
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-04-17 06:34:09 +0200

    refactor/better grouped routes

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+require __DIR__.'/auth.php';
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -25,14 +16,18 @@ Route::get('/', function () {
     ]);
 })->name('index');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])
+    ->group(function () {
+        Route::get('/dashboard', fn () => Inertia::render('Dashboard'))
+            ->middleware(['verified'])
+            ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+        Route::controller(ProfileController::class)
+            ->prefix('/profile')
+            ->name('profile.')
+            ->group(function () {
+                Route::get('/', 'edit')->name('edit');
+                Route::patch('/', 'update')->name('update');
+                Route::delete('/', 'destroy')->name('destroy');
+            });
+    });
