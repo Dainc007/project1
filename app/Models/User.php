64abb582773cfcb2fb3953bf4069a6361c7f4fd8
Daniel Heinze <daniel.he@modulesgarden.com>
2023-05-04 19:23:54 +0200

    User Model update

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations;
+
 
 class User extends Authenticatable
 {
@@ -43,4 +45,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function userAsTeam(): Relations\HasOne
+    {
+        return $this->hasOne(UserAsTeam::class);
+    }
 }
---
+++ database/seeders/prod/UserSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders\prod;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(100)->create();
+    }}
